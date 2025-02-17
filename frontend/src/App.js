6a6ebe9ddf96560c5cf52b973be25b258530043c
gokkull-15 <gokkull04@gmail.com>
2025-02-17 11:48:54 +0530

    app.js code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AccountPage from "./pages/AccountPage";
+import CartPage from "./pages/CartPage";
+import OrderPage from "./pages/OrderPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Help from "./components/Help";
+import { FaShoppingCart, FaUser, FaHome } from "react-icons/fa";
 
-const App = () => {
+export default function App() {
   return (
-    <div className=''>
-      Hello
-    </div>
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <main className="flex-1 p-6">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/order" element={<OrderPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/help" element={<Help />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-
-export default App
